f800ba8f0033a5215e4c2de8364524c472c79697
Mr Koios <karles.panagiotis@gmail.com>
2022-09-18 00:48:09 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
-
+app._static_folder = 'templates'
 
 @app.route("/")
 def hello_world():
@@ -14,4 +14,4 @@ def data():
     return jsonify(testData)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=8080)
